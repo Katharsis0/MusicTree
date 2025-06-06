918cafbea8ac49d8e5de8b7894d7e872862b7763
yaritzaelena <48070919+yaritzaelena@users.noreply.github.com>
2025-06-06 00:14:28 -0600

    back and front

--- musictree-frontend/src/VistaCurador/MenuCurador.jsx
+++ musictree-frontend/src/VistaCurador/MenuCurador.jsx
@@ -1,32 +1,35 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const roles = [
-{ name: "Crear Clúster", path: "/curador/crearcluster" },
-{ name: "Ver Clústers", path: "/curador/vercluster" },
-{ name: "Crear Género/Subgénero", path: "/curador/creargenero" }
+const opciones = [
+  { name: "Crear Clúster", path: "/curador/crearcluster" },
+  { name: "Ver Clústers", path: "/curador/vercluster" },
+  { name: "Crear Género/Subgénero", path: "/curador/creargenero" },
+  { name: "Importar Géneros", path: "/curador/importargeneros" },
+  { name: "Crear Artista", path: "/curador/crearartista" },
+  { name: "Ver Catálogo de Artistas", path: "/curador/catalogoartistas" }
 ];
 
 const MenuCurador = () => {
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div className="container text-center mt-5">
-            <h2>Seleccione su tipo de usuario</h2>
-                <div className="row justify-content-center mt-4">
-                {roles.map((role, index) => (
-                <div className="col-md-4 mb-3" key={index}>
-                <button
-                    className="btn btn-primary w-100"
-                    onClick={() => navigate(role.path)}
-                >
-                {role.name}
-                </button>
-                </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="container text-center mt-5">
+      <h2>Menú del Curador</h2>
+      <div className="row justify-content-center mt-4">
+        {opciones.map((opcion, index) => (
+          <div className="col-md-4 mb-3" key={index}>
+            <button
+              className="btn btn-primary w-100"
+              onClick={() => navigate(opcion.path)}
+            >
+              {opcion.name}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuCurador;
